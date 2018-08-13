7791bdf346157513b8febeec65427a2a2610a14e
Adam Dzierzko <dzierzko.a@gmail.com>
2018-08-12 23:50:33 -0400

    Add GET routes for geting all posts and post by id, add DELETE route for post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,8 +4,34 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+
 
 const validatePostInput = require('../../validation/post');
+
+// @route   GET api/posts
+// @desc    get posts
+// @access  Public
+router.get('/', (req, res) => {
+
+    Post.find()
+        .sort({date: -1})
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({nopostsfound: 'No posts found'}));
+});
+
+// @route   GET api/posts/:id
+// @desc    get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err =>
+            res.status(404).json({nopostfound: 'No post found with provided ID'})
+        );
+});
+
 
 // @route GET api/posts/test
 // @desc test posts route
@@ -36,3 +62,27 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 
     newPost.save().then(post => res.json(post))
 });
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+        Profile.findOne({user: req.user.id}).then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    // Check for post owner
+                    if (post.user.toString() !== req.user.id) {
+                        return res
+                            .status(401)
+                            .json({notauthorized: 'User not authorized'});
+                    }
+
+                    post.remove().then(() => res.json({success: true}));
+                })
+                .catch(err => res.status(404).json({postnotfound: 'No post found'}));
+        });
+    }
+);
+
+module.exports = router;
