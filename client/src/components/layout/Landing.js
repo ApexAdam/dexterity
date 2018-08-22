06148b328d72fd6312a8aea9c5110d5fbdf48610
Adam Dzierzko <dzierzko.a@gmail.com>
2018-08-21 20:42:15 -0400

    Add TexFieldGroup component and refactor code

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,7 +8,7 @@ class Landing extends Component {
 
     componentDidMount() {
         if (this.props.auth.isAuthenticated) {
-            this.props.history.push('/');
+            this.props.history.push('/dashboard');
         }
     }
 
