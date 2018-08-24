29923d19f4c055adb116642937f3d84aa3ea547f
Adam Dzierzko <dzierzko.a@gmail.com>
2018-08-23 21:22:53 -0400

    Add logic and basic html to Dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,26 +1,72 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import {getCurrentProfile} from "../../actions/profileActions";
+import Spinner from '../common/Spinner'
 
 class Dashboard extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.getCurrentProfile()
     }
 
     render() {
+
+        const {user} = this.props.auth;
+        const {profile, loading} = this.props.profile;
+
+        let dashboardContent;
+
+        if(profile === null || loading){
+            dashboardContent = <Spinner/>
+        }else{
+            if(Object.keys(profile).length > 0){
+                dashboardContent = <h4>Display profile</h4>
+            }else{
+                // User has no profile
+                dashboardContent = (
+                    <div>
+                        <p className="lead text-muted">
+                            Welcome {user.name}
+                        </p>
+                        <p>You have not created a profile</p>
+                        <Link to="/create/profile" className=""> Create Profile </Link>
+                    </div>
+                )
+            }
+        }
         return (
             <div>
-                
+                <div className="dashboard">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h1 className="display-4">
+                                    Dashboard
+                                </h1>
+                                {dashboardContent}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-function mapStateToProps(state) {
-    return {};
-}
+Dashboard.propTyes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    profile: state.profile,
+    auth: state.auth
+
+});
 
 export default connect(
     mapStateToProps, {getCurrentProfile}
